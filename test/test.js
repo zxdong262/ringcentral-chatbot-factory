cf2cb31b2766c40786ce4d6f8c032c9b17167a56
ZHAO Xudong <zxdong@gmail.com>
2019-02-15 09:16:51 +0800

    Support js/python templates

--- test/test.js
+++ test/test.js
@@ -13,9 +13,19 @@ const testConfig = {
   confirm: true
 }
 
+const testConfig1 = {
+  name: 'my-app1',
+  npmName: 'my-app1',
+  description: 'ma-app1',
+  version: '0.0.1',
+  language: 'js',
+  confirm: true
+}
+
+
 describe(pack.name, function() {
   this.timeout(100000)
-  it('default', function(done) {
+  it('python', function(done) {
     prompts.inject(testConfig)
     let p = resolve(__dirname, './my-app')
     rcf({
@@ -40,4 +50,28 @@ describe(pack.name, function() {
     }, 5500)
   })
 
+  it('js', function(done) {
+    prompts.inject(testConfig1)
+    let p = resolve(__dirname, './my-app1')
+    rcf({
+      path: p
+    })
+    setTimeout(() => {
+      let pkg = resolve(p, 'package.json')
+      let pkgObj = require(pkg)
+      assert(pkgObj.version === testConfig1.version)
+      assert(pkgObj.name === testConfig1.name)
+      assert(pkgObj.description === testConfig1.description)
+      let em = resolve(p, 'README.md')
+      let str = require('fs').readFileSync(em).toString()
+      assert(
+        str.includes(`# ${testConfig1.name}`)
+      )
+      assert(
+        str.includes(`${testConfig1.description}`)
+      )
+      rm('-rf', p)
+      done()
+    }, 5500)
+  })
 })
